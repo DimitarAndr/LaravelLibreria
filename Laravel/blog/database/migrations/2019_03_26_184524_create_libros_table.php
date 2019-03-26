1bc022b30e4b27c1aa89b38ecd67143c18b39264
Dimitar Andreev <cf17Dimiter.andreev@iesjoandaustria.org>
2019-03-26 20:48:37 +0100

    Commiting Project Laravel Libreria 26.03 from school part DDBB and migrations

---
+++ Laravel/blog/database/migrations/2019_03_26_184524_create_libros_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLibrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('year',8);
+            $table->string('escritor',64);
+            $table->boolean('rented')->default(false);
+            $table->text('synopsis');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libros');
+    }
+}
