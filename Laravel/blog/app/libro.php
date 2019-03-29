8f995ee2d35904953821a80e550556cdef3c6f4c
Dimitar <hian@abv.bg>
2019-03-29 14:41:06 +0100

    Commiting Part 3 from Practica Laravel Library 29.03.2019

--- Laravel/blog/app/libro.php
+++ Laravel/blog/app/libro.php
@@ -4,7 +4,19 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class libro extends Model
+class Libro extends Model
 {
-    //
+   public static function listAllLibros(){
+       $titles=[];
+       $libros = Libro::all('title');
+       foreach ($libros as $libro){
+         array_push( $titles,$libro);
+       }
+        return($titles);
+    }
+
+    public static function showLibro($id){
+       $libroElegido=Libro::findOrFail($id);
+       return($libroElegido);
+    }
 }
